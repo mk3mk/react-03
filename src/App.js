6baf2aa68cd444cf72ebf6ceedefe848c8e52536
mk3mkmk3mk <20783129-mk3mkmk3mk@users.noreply.replit.com>
2024-01-31 13:02:35 +0000

    111

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.dark-site.ru/users/")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div className="App">
+      <div>
+        <h1>Our users</h1>
+        {users.map((user) => (
+          <div key={user.id} to={`/users/${user.id}`}>
+            <li>username: {user.username}</li>
+            more
+            <hr />
+          </div>
+        ))}
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
