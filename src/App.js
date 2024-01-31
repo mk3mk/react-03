090cc32b5918a72366c2010aba48e9d5cae26efe
Replit user <>
2024-01-31 13:50:56 +0000

    112

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import "./App.css";
 import { useState, useEffect } from "react";
 
@@ -12,6 +16,36 @@ function App() {
 
   return (
     <div className="App">
+      <Container>
+        {/* Stack the columns on mobile by making one full-width and the other half-width */}
+        <Row>
+          <Col xs={12} md={8}>
+            xs=12 md=8
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
+
+        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+        <Row>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
+
+        {/* Columns are always 50% wide, on mobile and desktop */}
+        <Row>
+          <Col xs={6}>xs=6</Col>
+          <Col xs={6}>xs=6</Col>
+        </Row>
+      </Container>
       <div>
         <h1>Our users</h1>
         {users.map((user) => (
