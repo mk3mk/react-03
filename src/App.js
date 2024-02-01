e4ee62da3b43de96aecd8bec65a0b408b81bb64b
Replit user <>
2024-02-01 13:50:49 +0000

    01022024-01

--- src/App.js
+++ src/App.js
@@ -18,61 +18,60 @@ function App() {
     <div className="App">
       <Container>
         {/* Stack the columns on mobile by making one full-width and the other half-width */}
-        <Row>
-          <Col xs={12} md={8}>
-            xs=12 md=8
+
+        <Row className="row">
+          <Col className="col" xs={12} md={8}>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
           </Col>
-          <Col xs={6} md={4}>
-            xs=6 md=4
+          <Col className="col" xs={6} md={4}>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
           </Col>
         </Row>
 
         {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-        <Row>
-          <Col xs={6} md={4}>
+        <Row className="row">
+          <Col className="col" xs={6} md={4}>
             xs=6 md=4
           </Col>
-          <Col xs={6} md={4}>
-            xs=6 md=4
+          <Col className="col" xs={6} md={4}>
+            <div>
+              <h1>Our users</h1>
+              {users.map((user) => (
+                <div key={user.id} to={`/users/${user.id}`}>
+                  <li>username: {user.username}</li>
+                  more
+                  <hr />
+                </div>
+              ))}
+            </div>
           </Col>
-          <Col xs={6} md={4}>
+          <Col className="col" xs={6} md={4}>
             xs=6 md=4
           </Col>
         </Row>
 
         {/* Columns are always 50% wide, on mobile and desktop */}
-        <Row>
-          <Col xs={6}>xs=6</Col>
-          <Col xs={6}>xs=6</Col>
+        <Row className="row">
+          <Col className="col" xs={6}>
+            xs=6
+          </Col>
+          <Col className="col" xs={6}>
+            xs=6
+          </Col>
         </Row>
       </Container>
-      <div>
-        <h1>Our users</h1>
-        {users.map((user) => (
-          <div key={user.id} to={`/users/${user.id}`}>
-            <li>username: {user.username}</li>
-            more
-            <hr />
-          </div>
-        ))}
-      </div>
-
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
