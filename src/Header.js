93091b6b649f4606176eed97cf3e37fbdf7a1dfa
mk3mk <mk3mk@yandex.ru>
2024-02-07 22:43:37 +0300

    0702

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import ReactLogo from './logo.svg';
 
 import "./App.css";
 
@@ -14,31 +14,18 @@ function App() {
         {/* Stack the columns on mobile by making one full-width and the other half-width */}
 
         <Row className="row">
-          <Col className="col" xs={12} md={4}>
+          <Col className="col" xs={12} md={2}>
+            <img src={ReactLogo} alt="React Logo" width="50"/>
+          </Col>
+          <Col className="col" xs={12} md={8}>
             <p>Phone:</p>
           </Col>
-          <Col className="col" xs={12} md={4}>
-            <p>Phone:</p>
-          </Col>
-          <Col className="col" xs={12} md={4}>
+          <Col className="col" xs={12} md={2}>
             <p>Phone:</p>
           </Col>
         </Row>
 
-        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-        <Row className="row">
-          <Col className="col" xs={12} md={4}>
-            xs=6 md=4
-          </Col>
-          <Col className="col color-secondary" xs={12} md={4}>
-            <div>
-              <h1>HEADER</h1>
-            </div>
-          </Col>
-          <Col className="col" xs={12} md={4}>
-            xs=6 md=4
-          </Col>
-        </Row>
+
 
         {/* Columns are always 50% wide, on mobile and desktop */}
         <Row className="row">
@@ -55,6 +42,25 @@ function App() {
             </Navbar>
           </Col>
         </Row>
+
+
+
+        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+        <Row className="row header-row">
+          <Col className="col" xs={12} md={8}>
+            <div>
+              <h1>HEADER</h1>
+            </div>
+          </Col>
+          <Col className="col" xs={12} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
+
+
+
+
+
       </Container>
     </div>
   );
